fee49f92f4a35f574f1f09e88906e41761b33b4f
nacho <nacho@nacho-VirtualBox>
2017-05-26 16:57:27 +0200

    master commit1

--- practica_m8/newfile.php
+++ practica_m8/newfile.php
@@ -13,7 +13,7 @@ class LoginController extends Controller {
 		include ("views/login.tpl.php");
 	}
 	
-	
+	// modificacion en el master
 	public function validar(){
 		include("bootstrap.php");
 		
